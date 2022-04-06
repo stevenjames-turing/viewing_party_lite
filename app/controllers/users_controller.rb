6943d6200068441b61a3f0408cf814aa7a840958
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-06 17:02:41 -0500

    Refactor: Update to allow for new password attribute

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create!(name: params[:name], email: params[:email])
+    user = User.create!(user_params)
     redirect_to "/users/#{user.id}"
+  end
+
+  private 
+
+  def user_params
+    params.permit(:name, :email, :password)
   end
 end
