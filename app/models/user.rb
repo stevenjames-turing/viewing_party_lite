dcb28bbbfde98c5a148114c5dfdcb00644f1ed10
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-06 16:50:05 -0500

    Feat: Add password_digest to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   has_many :user_parties
   has_many :viewing_parties, through: :user_parties
 
-  validates_presence_of :name, :email
+  validates_presence_of :name, :email, :password_digest
+  validates_uniqueness_of :email 
+
+  has_secure_password
 end
